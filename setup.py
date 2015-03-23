b324770c3772aa8ca8227012d55ba9fc732e1e53
tzp5vg <tzp5vg@virginia.edu>
2015-03-23 16:39:56 -0400

    Run test site

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='GasApplication',
+    version='1.0',
+    packages=['untitled3'],
+    url='',
+    license='',
+    author='Teddy Peneva,Caroline Dahmen, Apurva Kasanagottu',
+    author_email='',
+    description='CS4753: Initial web site for gas application'
+)
